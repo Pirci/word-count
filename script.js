3eecf9913c92bea4e9589ee0f58f362e7835a96c
Pirci <emrepirci@gmail.com>
2021-12-22 00:33:00 +0100

    App logic upgrade

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let btn = document.getElementById('btn');
-let output = document.getElementById('output');
+let outputCharacters = document.getElementById('outputCharacters');
+let outputWords = document.getElementById('outputWords');
 
 btn.addEventListener('click', function () {
-  let str = document.getElementById('word').value;
-  output.innerHTML = str.length;
+	let str = document.getElementById('character').value;
+	outputCharacters.innerHTML = str.length;
+});
+
+btn.addEventListener('click', function () {
+	let str = document.getElementById('character').value;
+	let str2 = str.trim();
+	let str3 = str2.split(' ');
+	outputWords.innerHTML = str3.length;
 });
